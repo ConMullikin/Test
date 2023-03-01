fd3a2e6c0c3676aff575f1058e090c5e3677dbf4
matthew-hase-00 <HaseMG22@gcc.edu>
2023-03-01 10:39:48 -0500

    Revised the print statement to take input from a user for their name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Scanner inScan = new Scanner(System.in);
+        String name = inScan.next();
+        System.out.println("Hello " + name);
     }
 }
